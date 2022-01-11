2b04a549e39f16043a0ec5d8a841d777d3e34ea1
Panagiotis Skarlas <panossk2@gmail.com>
2022-01-11 23:38:25 +0200

    add basic sentry integration

--- src/utils/ytdl.js
+++ src/utils/ytdl.js
@@ -1,5 +1,6 @@
 const dytdl = require('discord-ytdl-core');
 const songControls = require('./songControls')
+const Sentry = require("@sentry/node");
 
 module.exports = (input, server, extraParams) => {
   return dytdl(input, {
@@ -10,6 +11,7 @@ module.exports = (input, server, extraParams) => {
   }).on('error', (error) => {
     server.channel.send(`There was an error while playing this song.`);
     // songControls.nextSong(server);
+    Sentry.captureException(error);
     console.log(error)
   })
 }
